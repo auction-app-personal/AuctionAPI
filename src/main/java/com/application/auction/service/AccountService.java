de78617c0e26f32022e1a53bd18a969871f8aaf7
Maksym Shyiko <maxymshyjko@gmail.com>
2024-10-18 15:32:04 +0100

    Implemented AccountService

--- src/main/java/com/application/auction/service/AccountService.java
+++ src/main/java/com/application/auction/service/AccountService.java
@@ -1,9 +1,13 @@
 package com.application.auction.service;
 
 import com.application.auction.dao.AccountDAO;
+import com.application.auction.exceptions.ResourceNotFoundException;
 import com.application.auction.model.account.Account;
+import com.application.auction.model.account.AccountDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AccountService {
@@ -14,7 +18,32 @@ public class AccountService {
         this.accountDAO = accountDAO;
     }
 
-    public Account getAccount(Long accountId) {
-        return accountDAO.findById(accountId).orElse(null);
+    public AccountDTO getAccount(Long accountId) {
+        Account account = accountDAO.findById(accountId).orElse(null);
+        return account != null ? new AccountDTO(account) : null;
+    }
+
+    public List<AccountDTO> getAllAccounts() {
+        List<Account> accounts = (List<Account>) accountDAO.findAll();
+        return accounts.stream().map(AccountDTO::new).toList();
+    }
+
+    public void updateAccount(Long id, AccountDTO accountDTO) {
+        Account account = accountDAO.findById(id).orElse(null);
+        if (account == null) {
+            throw new ResourceNotFoundException("Account with id " + id + " not found");
+        }
+        account.setName(accountDTO.getName());
+        account.setEmail(accountDTO.getEmail());
+        account.setRole(accountDTO.getRole());
+        accountDAO.save(account);
+    }
+
+    public void deleteAccount(Long id) {
+        Account account = accountDAO.findById(id).orElse(null);
+        if (account == null) {
+            throw new ResourceNotFoundException("Account with id " + id + " not found");
+        }
+        accountDAO.delete(account);
     }
 }
