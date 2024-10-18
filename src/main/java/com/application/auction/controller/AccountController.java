67afb51a7f2e4402ce83a6bcfcafbb6210663f65
Maksym Shyiko <maxymshyjko@gmail.com>
2024-10-18 15:31:43 +0100

    Implemented AccountService

--- src/main/java/com/application/auction/controller/AccountController.java
+++ src/main/java/com/application/auction/controller/AccountController.java
@@ -1,10 +1,45 @@
 package com.application.auction.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.application.auction.model.account.AccountDTO;
+import com.application.auction.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/accounts")
 public class AccountController {
-    //Manage operations with AccountData
+    private final AccountService accountService;
+
+    @Autowired
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<AccountDTO>> getAllAccounts() {
+        List<AccountDTO> accounts = accountService.getAllAccounts();
+        return new ResponseEntity<>(accounts, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AccountDTO> getAccountById(@PathVariable("id") Long id) {
+        AccountDTO accountDTO = accountService.getAccount(id);
+        return new ResponseEntity<>(accountDTO, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateAccount(@PathVariable("id") Long id, @RequestBody AccountDTO accountDTO) {
+        accountService.updateAccount(id, accountDTO);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteAccount(@PathVariable("id") Long id) {
+        accountService.deleteAccount(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
